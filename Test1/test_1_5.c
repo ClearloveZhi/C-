d23393e1228d0a0988fe21af2143cfc3e97624ee
ClearloveZhi <84669451+ClearloveZhi@users.noreply.github.com>
2022-07-26 20:07:26 +0800

    Add files via upload

---
+++ Test1/test_1_5.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < i; j++)
+//		{
+//			printf("%s"," ");
+//		}
+//		printf("%s\n","*****");
+//	}
+//	return 0;
+//}
---
+++ Test1/test_1_6.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int max = 0;
+//	scanf("%d%d%d",&a,&b,&c);
+//	if (a > b)
+//	{
+//		max = a;
+//	}
+//	else
+//	{
+//		max = b;
+//	}
+//	if (c > max)
+//	{
+//		max = c;
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
