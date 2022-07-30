b96b8f33633fa4b26662c91804b44bac0ab9bda4
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-07-30 22:30:04 +0800

    Add files via upload

---
+++ Test4/test_4_1.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	double a, b, c, disc, x1, x2,p,q;
+//	scanf("%lf%lf%lf", &a, &b, &c);
+//	disc = b * b - 4 * a * c;
+//	if (disc < 0)
+//		printf("方程无实根\n");
+//	else
+//	{
+//		p = -b / (2.0 * a);
+//		q = sqrt(disc) / (2.0 * a);
+//		x1 = p + q;
+//		x2 = p - q;
+//		printf("x1=7.2%f\n,x2=%7.2f\n", x1, x2);
+//	}
+//
+//	return 0;
+//}
---
+++ Test4/test_4_2.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	float a, b, c;
+//	scanf("%f%f", &a, &b);
+//	if (a > b)
+//	{
+//		c = a;
+//		a = b;
+//		b = c;
+//	}
+//	printf("%5.2f,%5.2f\n", a, b);
+//	return 0;
+//}
