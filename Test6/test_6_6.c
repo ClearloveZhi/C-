464bf57d2add9d3184808f21fd2d7951a17eda16
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-08-10 21:06:38 +0800

    Add files via upload

---
+++ Test6/test_6_6.c
@@ -0,0 +1,12 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	char c[15] = { 'I',' ','a','m',' ','a',' ','b','o','y' };
+//	int i;
+//	for (i = 0; i < 15; i++)
+//	{
+//		printf("%c", c[i]);
+//	}
+//	return 0;
+//}
---
+++ Test6/test_6_8.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	char string[81];
+//	int i, num = 0, word = 0;
+//	char c;
+//	gets(string);
+//	for (i = 0; (c = string[i])!= '\0'; i++)
+//		if (c == ' ')
+//			word = 0;
+//		else if (word == 0)
+//		{
+//			word = 1;
+//			num++;
+//		}
+//	printf("There are %d words in this line.", num);
+//	return 0;
+//}
