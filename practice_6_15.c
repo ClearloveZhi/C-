12001610c9292dc48bcc304df8124c86f4ab746b
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-08-17 21:50:22 +0800

    Add files via upload

---
+++ practice_6_15.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	char s1[100] = { 0 };
+//	char s2[50] = { 0 };
+//	int index1 = 0;
+//	int index2 = 0;
+//	printf("请输入字符串2：");
+//	scanf("%s", &s2);
+//	// 2. 将s2中的字符逐个往s1之后拼接
+//	while (s1[index1++] = s2[index2++]);
+//	printf("将s2拷贝到s1中:%s", s1);
+//	return 0;
+//}
