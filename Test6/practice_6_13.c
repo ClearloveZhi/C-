bd1a378b0e5e68709773fdf509cb7eff66643215
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-08-17 22:19:09 +0800

    Add files via upload

---
+++ Test6/practice_6_13.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	char s1[100]={0};
+//	char s2[50] = { 0 };
+//	int index1 = 0;
+//	int index2 = 0;
+//	printf("请输入字符串1：");
+//	scanf("%s", &s1);
+//	printf("请输入字符串2：");
+//	scanf("%s", &s2);
+//	while ('\0' != s1[index1])
+//		index1++;
+//	// 2. 将s2中的字符逐个往s1之后拼接
+//	while (s1[index1++] = s2[index2++]);
+//
+//	printf("%s\n", s1);
+//	return 0;
+//}
