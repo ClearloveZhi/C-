958d80798e04f6c9fdd8c584cd4faa95b2f595f0
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-08-17 22:14:39 +0800

    Add files via upload

---
+++ Test7/test_7_1.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//void print_star()
+//{
+//	printf("*********************\n");
+//}
+//void print_play()
+//{
+//	printf("How do you do!\n");
+//}
+//int main()
+//{
+//	print_star();
+//	print_play();
+//	print_star();
+//	return 0;
+//}
---
+++ Test7/test_7_2.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int max(int a, int b)
+//{
+//	int z;
+//	z = a > b ? a : b;
+//	return z;
+//}
+//int main()
+//{
+//	int a, b, c;
+//	printf("请输入二个整数:");
+//	scanf("%d%d", &a, &b);
+//	c=max(a, b);
+//	printf("最大的数：%d", c);
+//	return 0;
+//}
