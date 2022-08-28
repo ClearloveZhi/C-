82bd9668b320ae800ee0afcbe868228aff50fe6c
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-08-28 22:53:34 +0800

    Add files via upload

---
+++ Test7/test_7_8.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//void move(char x, char y)
+//{
+//	printf("%c-->%c\n", x, y);
+//}
+//void hanoi(int n, char one, char two, char three)
+//{
+//	if (n == 1)
+//		move(one, three);
+//	else
+//	{
+//		hanoi(n - 1, one, three, two);
+//		move(one, three);
+//		hanoi(n - 1, two, one, three);
+//	}
+//}
+//int main()
+//{
+//	int m;
+//	printf("请输入盘的个数：");
+//	scanf("%d", &m);
+//	hanoi(m,'A','B','C');
+//	return 0;
+//}
---
+++ Test7/test_7_9.c
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//int main()
+//{
+//	int a[10];
+//	int m, n, i;
+//	printf("请输入10个整数：");
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	printf("\n");
+//	for (i = 1, m = a[0], n = 0; i < 10; i++)
+//	{
+//		if (max(m, a[i]) > m)
+//		{
+//			m = max(m, a[i]);
+//			n = i;
+//		}
+//	}
+//	printf("最大的数字是：%d,它的位置是：%d", m, n);
+//	return 0;
+//}
