35e37f32dd7c2eef527f0f534888219754dfa1d2
Clearlove7 <84669451+ClearloveZhi@users.noreply.github.com>
2022-08-28 22:53:48 +0800

    Add files via upload

---
+++ Test7/test_7_7.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int fac(int n)
+//{
+//	int f;
+//	if (n <= 0)
+//		printf("ÇëÖØÐÂÊäÈë£¡");
+//	else if (n == 1)
+//		f = 1;
+//	else
+//		f = fac(n) * n;
+//	return f;
+//		
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int m=fac(n);
+//	printf("n!=%d", m);
+//	return 0;
+//}
